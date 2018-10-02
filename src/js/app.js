3aba6bbbf2b28980f96a30e5e4394431ca7d81bf
Thomas Petracco <thomas@petrac.co>
2018-10-02 14:29:49 +0300

    Add download button

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,28 @@
-//
+window.JSZip = require('jszip');
+window.FileSaver = require('file-saver');
+window.Vue = require('vue');
+window.axios = require('axios');
+
+new Vue({
+  el: '#app',
+
+  methods: {
+    download(template) {
+        var zip = new JSZip();
+
+        axios.all([
+            axios.get('templates/' + template + '.html'),
+            axios.get('templates/' + template + '.css'),
+          ])
+          .then(axios.spread((html, css, appCss) => {
+                zip.file(template + ".html", html.data);
+                zip.file(template + ".css", css.data);
+
+                zip.generateAsync({type:"blob"})
+                    .then(function (blob) {
+                        FileSaver.saveAs(blob, template + ".zip");
+                    });
+          }));
+    }
+  }
+});
